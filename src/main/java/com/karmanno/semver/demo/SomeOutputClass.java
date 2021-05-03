656fc2336708e40611c0da35419c617affc07637
karmann-dm <karmanno@gmail.com>
2021-05-03 16:27:11 +0200

    fix: Add debug line

--- src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
+++ src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
@@ -3,5 +3,6 @@ package com.karmanno.semver.demo;
 public class SomeOutputClass {
     public static void main(String[] args) {
         System.out.println("Output");
+        System.out.println("Enable output");
     }
 }
