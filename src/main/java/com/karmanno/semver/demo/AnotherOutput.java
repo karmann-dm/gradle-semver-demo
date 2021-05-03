5d98bd0fe1b649c6f0f604cd4f2ba2ae223ca9ac
karmann-dm <karmanno@gmail.com>
2021-05-03 16:32:08 +0200

    global: Add another output class

---
+++ src/main/java/com/karmanno/semver/demo/AnotherOutput.java
@@ -0,0 +1,10 @@
+package com.karmanno.semver.demo;
+
+import java.util.Arrays;
+
+public class AnotherOutput {
+    public static void main(String[] args) {
+        Arrays.stream(args)
+                .forEach(arg -> System.out.println(arg.toUpperCase()));
+    }
+}
