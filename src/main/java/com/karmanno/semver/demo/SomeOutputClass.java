94da0f97d484fd3ee1a26e058fdcafec29092759
karmann-dm <karmanno@gmail.com>
2021-05-03 16:27:44 +0200

    feat: Add javadocs

--- src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
+++ src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
@@ -1,6 +1,10 @@
 package com.karmanno.semver.demo;
 
 public class SomeOutputClass {
+    /**
+     * Javadocs
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Output");
         System.out.println("Enable output");
