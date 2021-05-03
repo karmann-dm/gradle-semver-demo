d17296b3bf596d4c7b1a8f465fb75aa7f2b13d87
karmann-dm <karmanno@gmail.com>
2021-05-03 16:28:54 +0200

    fix: Add args debug info

--- src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
+++ src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
@@ -1,5 +1,7 @@
 package com.karmanno.semver.demo;
 
+import java.util.Arrays;
+
 public class SomeOutputClass {
     /**
      * Javadocs
@@ -8,5 +10,6 @@ public class SomeOutputClass {
     public static void main(String[] args) {
         System.out.println("Output");
         System.out.println("Enable output");
+        System.out.println("Input args " + Arrays.toString(args));
     }
 }
