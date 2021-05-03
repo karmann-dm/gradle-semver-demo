4bc96c53bf327f0de1969da02d787d58650dde87
karmann-dm <karmanno@gmail.com>
2021-05-03 16:29:59 +0200

    fix: Fix javadocs

--- src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
+++ src/main/java/com/karmanno/semver/demo/SomeOutputClass.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SomeOutputClass {
     /**
      * Javadocs
-     * @param args
+     * @param args input arguments
      */
     public static void main(String[] args) {
         System.out.println("Output");
